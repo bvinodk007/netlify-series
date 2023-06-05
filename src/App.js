1c4c28a3098e59bc65a76f79e20dd376d22f4cc0
Vinod Kumar Bainapalli <bainapallivinodkumar@gmail.com>
2023-06-05 17:59:44 +0530

    publish actions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React netlify series</h1>
+      <h1>React netlify series with Actions</h1>
     </div>
   );
 }
